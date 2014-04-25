36c58a2ffbe775efa0c58938b51fc84ae1d964ff
AkBKukU <shelbyjueden@gmail.com>
2014-04-24 21:07:54 -0700

    Commented

--- clipb.cpp
+++ clipb.cpp
@@ -8,32 +8,25 @@ const std::string clipbData = "/tmp/clipb";
 
 void saveClip (std::string text);
 std::string loadClip ();
+std::string getInput ();
 
+/* Main
+ *
+ */
 int main (int argc, char *argv[])
 {	
-for (int i = 1; i < argc; i++) { /* We will iterate over argv[] to get the parameters stored inside.
-                                      * Note that we're starting on 1 because we don't need to know the 
-                                      * path of the program, which is stored in argv[0] */
-        if (i < argc) // Check that we haven't finished parsing already
-        {
-            if (std::string(argv[i]) == "-i") 
-            {
-                // Capture passed input as cin
-				std::string temp;
-				std::string input;
-				// Parse multiple lines
-				while(std::cin)
-				{
-				    std::getline (std::cin,temp);
-				    input = input + "\n" + temp;
-				}
-				input = input.substr(1);
+	// Parse Arguments
+	for (int i = 1; i < argc; i++) {
 
-				saveClip(input);
-            }
-    	}
+		// Check if inputing
+        if (std::string(argv[i]) == "-i") 
+        {
+            saveClip(getInput());
+        }
+    	
     }
 
+    // If no args the print clip board
     if (argc == 1)
     {
 		std::cout << loadClip();
@@ -43,6 +36,35 @@ for (int i = 1; i < argc; i++) { /* We will iterate over argv[] to get the param
 	return 0;
 }
 
+/* getInput
+ *
+ * Gets input from stream buffer to grap piped text
+ *
+ * @return std::string String of text passed in std::scin
+ */
+std::string getInput ()
+{
+
+	// Capture passed input as cin
+	std::string temp;
+	std::string input;
+	// Parse multiple lines
+	while(std::cin)
+	{
+	    std::getline (std::cin,temp);
+	    input = input + "\n" + temp;
+	}
+	// Remove last newline
+	input = input.substr(1);
+
+	return input;
+}
+
+/* saveClip
+ *
+ * Save input to temporary file
+ *
+ */
 void saveClip (std::string text)
 {
 
@@ -52,13 +74,19 @@ void saveClip (std::string text)
 	outFile << text;
 }
 
-
+/* loadClip
+ *
+ * Gets text from temporary file and returns it
+ *
+ * @return std::string String of text in temp file
+ */
 std::string loadClip ()
 {
     std::ifstream fileIn;
-    fileIn.open( "/tmp/clipb" , std::ifstream::in);
+    fileIn.open( "/tmp/clipb" );
     std::string output = "";
 
+    // Test if file exists
     if ( fileIn.good() )
     {
 		while (! fileIn.eof()){
@@ -68,5 +96,7 @@ std::string loadClip ()
 		}
 	}
 	fileIn.close();
+
+	// Crop off EOF char and return text
 	return output.substr(0,output.length()-1);
 }
